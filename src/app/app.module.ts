4aa76f585a1e066ca80f88037b41957c69f5fe52
Denis Tankov <denis.tankov@eltex.loc>
2020-07-28 19:47:52 +0700

    temp commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { Routes, RouterModule } from '@angular/router';
+//import { LoginPageComponent } from '@/pages/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    //LoginPageComponent
   ],
   imports: [
     BrowserModule,
